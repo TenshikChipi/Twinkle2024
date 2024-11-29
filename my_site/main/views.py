7af957bbd76c9a6dd81fcbe8b1bee21fcdceab8d
sone00000000 <sonya.chekhova@gmail.com>
2024-11-29 18:26:10 +0300

    Update views.py

--- my_site/main/views.py
+++ my_site/main/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-def index(request):
-    return render(request, "main/index.html")
-def autumn(request):
-    return render(request, "main/autumn.html")
-def colottype(request):
-    return render(request, "main/colottype.html")
+def aboutus(request):
+    return render(request, "main/aboutus.html")
+def colortp(request):
+    return render(request, "main/colortp.html")
+def colottypetest(request):
+    return render(request, "main/colottypetest.html")
 def log(request):
     return render(request, "main/log.html")
-def razdeli(request):
-    return render(request, "main/razdeli.html")
+def otcek(request):
+    return render(request, "main/otcek.html")
+def sign(request):
+    return render(request, "main/sign.html")
 def spring(request):
     return render(request, "main/spring.html")
 def summer(request):
     return render(request, "main/summer.html")
+def wardrob(request):
+    return render(request, "main/wardrob.html")
 def winter(request):
     return render(request, "main/winter.html")
