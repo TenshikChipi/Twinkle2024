4e8eb6e0287adbd09107b6260c9f1399be7f6944
sone00000000 <sonya.chekhova@gmail.com>
2024-11-29 18:30:29 +0300

    Update urls.py

--- my_site/main/urls.py
+++ my_site/main/urls.py
@@ -2,12 +2,15 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('',views.autumn),
-    path('',views.colottype), 
+    path('',views.aboutus),
+    path('',views.colortp),
+    path('',views.colottypetest), 
     path('',views.log),
-    path('',views.razdeli),
+    path('',views.menu),
+    path('',views.otcek),
+    path('',views.sign),
     path('',views.spring),
     path('',views.summer),
+    path('',views.wardrob),
     path('',views.winter),
-]    
+] 
